2184df037575b818b7fd5ac7f016c1471b2dde19
nicolae <nicolae@nicolaelaptop>
2019-09-28 12:55:36 -0400

    setup juint testing

---
+++ WeeklyAssignmentOneTakeTwo/tests/com/ss/dataaccess/tests/DataAccessObjectTest.java
@@ -0,0 +1,24 @@
+package com.ss.dataaccess.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class DataAccessObjectTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ WeeklyAssignmentOneTakeTwo/tests/com/ss/service/tests/ServiceTest.java
@@ -0,0 +1,24 @@
+package com.ss.service.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ServiceTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
